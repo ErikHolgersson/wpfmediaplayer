b210fa5500872985ab771578c31955608ef2ec12
Erik Wendler <s77224@smb.informatik.htw-dresden.de>
2019-11-06 17:21:26 +0100

    First commit

---
+++ WPFPlayer/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPlayer
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        wpfWrapper wpf;
+        bool fullScreen;
+        public MainWindow()
+        {
+            InitializeComponent();
+            wpf = new wpfWrapper(this, wpfpanel);
+            fullScreen = false;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //TOGGLE FULLSCREEN
+
+            fullScreen = !fullScreen;
+            wpf.SetFullscreen(fullScreen);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //PLAY
+            wpf.SetVolume(volSlider.Value);
+            wpf.SetBalance(balSlider.Value);
+            wpfpanel.Play();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //OPEN
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                wpf.Open(ofd.FileName);
+                this.Title = ofd.SafeFileName;
+                sliderTime.Maximum = wpf.GetDuration();
+            }
+
+        }
+
+        private void btn_pause_Click(object sender, RoutedEventArgs e)
+        {
+            //PAUSE
+            wpfpanel.Pause();
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            //STOP
+            wpfpanel.Close();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            wpf.SetVolume(e.NewValue);
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            wpf.SetBalance(e.NewValue);
+        }
+    }
+}
---
+++ WPFPlayer/wpfWrapper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFPlayer
+{
+    class wpfWrapper
+    {
+        MediaElement player;
+        MainWindow window;
+        public wpfWrapper(MainWindow windowHandle, MediaElement player) {
+            this.player = player;
+            window = windowHandle;
+        }
+        public void Open(Uri path, bool autoRun) {
+            player.Source = path;
+            if (autoRun)
+                Play();
+        }
+        public void Open(Uri path) { 
+            Open(path, true);
+        }
+        public void Open(string path, bool autoRun) { 
+            Open(new Uri(path, UriKind.RelativeOrAbsolute), autoRun); 
+        }
+        public void Open(string path) {
+            Open(new Uri(path, UriKind.RelativeOrAbsolute), true);
+        }
+        public void Play() {
+            if (player.Source != null) player.Play();
+        }
+        public void Pause() {
+            if (player.Source != null) player.Pause();
+        }
+        public void Stop() {
+            if (player.Source != null) player.Stop();
+        }
+        public void SetFullscreen(bool fullscreen) {
+            if (player.Source != null)
+            {
+                if (fullscreen)
+                {
+                    window.WindowStyle = WindowStyle.None;
+                    window.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    window.WindowStyle = WindowStyle.SingleBorderWindow;
+                    window.WindowState = WindowState.Normal;
+                }
+            }
+        }
+        public double GetPosition() {
+            if (player.Source != null && player.NaturalDuration.HasTimeSpan)
+                return player.Position.TotalSeconds;
+            return -1;
+        }
+        public void SetPosition(int position) {
+            if (player.Source != null)
+                player.Position = new TimeSpan(0, 0, position);
+        }
+        public double GetVolume() {
+            return player.Volume * 100;
+        }
+        public void SetVolume(double volume) {
+            player.Volume = volume / 100;
+        }
+        public double GetBalance() {
+            return player.Balance * 100;
+        }
+        public void SetBalance(double balance) {
+            player.Balance = balance / 100;
+        }
+        public double GetDuration() {
+            if (player.NaturalDuration.HasTimeSpan)
+                return player.NaturalDuration.TimeSpan.TotalSeconds;
+            else
+                return 0;
+        }
+
+    }
+}
